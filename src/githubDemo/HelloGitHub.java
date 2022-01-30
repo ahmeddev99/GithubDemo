e29ac3994d688c4fdbff109f2673a1064dd814c5
ahmeddev99 <rahmed.webdev@gmail.com>
2022-01-30 18:24:23 -0500

    first commit for GithubDemo

---
+++ src/githubDemo/HelloGitHub.java
@@ -0,0 +1,9 @@
+package githubDemo;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"I know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
+	}
+}
